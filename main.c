bea420eb2803a52e8a673cb87705a61b861f6e3b
Rafael Schmitz <rafael@hangar.digital>
2021-08-23 20:32:06 -0300

    [ AJUSTE ] E-mail

--- main.c
+++ main.c
@@ -55,12 +55,12 @@ int main() {
 				scanf("%s", &c1.email);
 				is_email = 0;
 				
-				if(strchr(c1.email, '@') != NULL || strchr(c1.email, '.') != NULL){
+				if(strchr(c1.email, '@') == NULL || strchr(c1.email, '.') == NULL){
 					is_email = 0;
 				}
 
 				if(is_email==0){
-					printf('Digite um e-mail válido!\n\n');
+					printf("Digite um e-mail válido!\n\n");
 				}
 
 			}while(is_email==0);
